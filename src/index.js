600f06a6abfc68a9cf176fa4ad5b07e676baacda
om singh <iamomsingh11@gmail.com>
2024-04-01 03:22:44 +0530

    added cors & cookie-parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+const PORT = process.env.PORT || 8000;
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed !!!!!!", error);
+  });
